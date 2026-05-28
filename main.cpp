9f947d0ab91f80f05a10c4432bda1c1b9c98aa01
progress <nikita15933@mail.ru>
2026-05-28 14:35:32 +0300

    feat: add enter number cell

--- main.cpp
+++ main.cpp
@@ -2,10 +2,8 @@
 #include <vector>
 #include <string>
 
-void field_rendering() 
+void field_rendering(std::vector<char>& cells) 
 {
-   std::vector<char> cells = { '1', '2', '3', '4', '5', '6', '7', '8', '9', };
-
    std::cout << cells[0] << " | " << cells[1] << " | " << cells[2] << "\n";   
    std::cout << "---+---+---\n";
    std::cout << cells[3] << " | " << cells[4] << " | " << cells[5] << "\n";   
@@ -13,9 +11,23 @@ void field_rendering()
    std::cout << cells[6] << " | " << cells[7] << " | " << cells[8] << "\n";   
 }
 
+char enter_cell()
+{
+   char number_cell;
+
+   std::cout << "Enter number cell: ";
+   std::cin >> number_cell;
+
+   return number_cell;
+}
+
 int main(int argc, const char** argv) {
    
-   field_rendering();
+   std::vector<char> cells = { '1', '2', '3', '4', '5', '6', '7', '8', '9', };
+   char number_cell;
    
+   field_rendering(cells);
+   number_cell = enter_cell();
+
    return 0;
 }
