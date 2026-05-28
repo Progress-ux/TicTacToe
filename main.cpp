1472a9c79082e185328aa1adec845970d4ebda2f
progress <nikita15933@mail.ru>
2026-05-28 14:25:12 +0300

    feat: add basic field rendering

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+void field_rendering() 
+{
+   std::vector<char> cells = { '1', '2', '3', '4', '5', '6', '7', '8', '9', };
+
+   std::cout << cells[0] << " | " << cells[1] << " | " << cells[2] << "\n";   
+   std::cout << "---+---+---\n";
+   std::cout << cells[3] << " | " << cells[4] << " | " << cells[5] << "\n";   
+   std::cout << "---+---+---\n";
+   std::cout << cells[6] << " | " << cells[7] << " | " << cells[8] << "\n";   
+}
 
 int main(int argc, const char** argv) {
    
-   std::cout << "Hello World";
+   field_rendering();
    
    return 0;
 }
