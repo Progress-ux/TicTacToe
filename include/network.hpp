4f805ddf31105275d4a59f7115d840286c759419
progress <nikita15933@mail.ru>
2026-05-28 19:30:46 +0300

    add network manager and client-server

---
+++ include/network.hpp
@@ -0,0 +1,53 @@
+#pragma once 
+#include <SFML/Network.hpp>
+#include <string>
+
+class NetworkManager 
+{
+protected: 
+   sf::TcpSocket socket;
+public:
+   virtual ~NetworkManager() = default;
+
+   void sendMove(int index) 
+   {
+      sf::Packet packet;
+      packet << index;
+      socket.send(packet);
+   }
+   int receiveMove()
+   {
+      sf::Packet packet;
+      if (socket.receive(packet) == sf::Socket::Status::Done) 
+      {
+         int index;
+         packet >> index;
+         return index;
+      }
+      return -1;
+   }
+};
+
+class Server : public NetworkManager
+{
+private:
+   sf::TcpListener listener;
+public:
+   bool start(unsigned short port)
+   {
+      if (listener.listen(port) != sf::Socket::Status::Done) return false;
+      return listener.accept(socket) == sf::Socket::Status::Done;
+   }
+
+};
+
+class Client : public NetworkManager
+{
+public:
+   bool connect(const std::string& ipString, unsigned short port) 
+   {
+      sf::IpAddress ip = sf::IpAddress::resolve(ipString).value_or(sf::IpAddress::Any);
+      return socket.connect(ip, port) == sf::Socket::Status::Done;
+   }
+
+};
