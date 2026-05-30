c1c75ab089b476ad8669a6f7865ce28c24469f65
progress <nikita15933@mail.ru>
2026-05-30 22:14:39 +0300

    feat(main): add load config

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "game_modes.hpp"
+#include "config_manager.hpp"
 
 int main(int argc, const char** argv) 
 {
+   ConfigManager::getInstance().load();
    GameMode::runMultiplayerGame();
    return 0;
 }
