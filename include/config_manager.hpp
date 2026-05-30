69586620f2f3ae472f5c1c8c0f0a3b7713fa2434
progress <nikita15933@mail.ru>
2026-05-30 21:26:07 +0300

    feat: add getters and setters

--- include/config_manager.hpp
+++ include/config_manager.hpp
@@ -7,14 +7,23 @@ class ConfigManager
 private:
    std::string serverIp;
    unsigned short serverPort;
+   float timeout;
 
    ConfigManager() = default;
 public:
    ConfigManager(const ConfigManager&) = delete;
    ConfigManager& operator=(const ConfigManager&) = delete;
+   ~ConfigManager();
 
    static ConfigManager& getInstance();
 
-   ~ConfigManager();
+   std::string getServerIp() const { return serverIp; }
+   void setServerIp(const std::string& ip) { serverIp = ip; }
+
+   unsigned short getServerPort() const { return serverPort; }
+   void setServerPort(unsigned short port) { serverPort = port; }
+
+   float getTimeout() const { return timeout; }
+   void setTimeout(float timeout) { this->timeout = timeout; }
 };
 
