6da177760b25b606cdfb867eedd180426421e907
progress <nikita15933@mail.ru>
2026-05-30 21:58:07 +0300

    feat(config): add load config

--- src/config_manager.cpp
+++ src/config_manager.cpp
@@ -1,5 +1,10 @@
 #include "config_manager.hpp"
 
+#include <nlohmann/json.hpp>
+#include <fstream>
+
+using json = nlohmann::json;
+
 ConfigManager &ConfigManager::getInstance()
 {
    static ConfigManager instance;
@@ -8,6 +13,17 @@ ConfigManager &ConfigManager::getInstance()
 
 void ConfigManager::load()
 {
+   std::fstream file("config.json");
+   if (!file.is_open())
+   {
+      resetToDefault();
+      return;
+   }
+   json data = json::parse(file);
+   
+   serverIp = data.value("serverIp", "127.0.0.1");
+   serverPort = data.value("serverPort", 53000);
+   timeout = data.value("timeout", 15);
 }
 
 void ConfigManager::save()
