3a6956317256c4e9cc18db3433f86120d3a2327d
progress <nikita15933@mail.ru>
2026-06-02 19:53:53 +0300

    feat: add show main menu

--- src/menu_manager.cpp
+++ src/menu_manager.cpp
@@ -1,9 +1,14 @@
 #include "menu_manager.hpp"
 
-
+#include <iostream>
 
 void MenuManager::showMainMenu()
 {
+   std::cout << "=== Tic Tac Toe ===\n";
+   std::cout << "1. Play Single Player (under development)\n";
+   std::cout << "2. Play Multi Player\n";
+   std::cout << "3. Settings\n";
+   std::cout << "0. Exit\n";
 }
 
 void MenuManager::showSettingsMenu()
