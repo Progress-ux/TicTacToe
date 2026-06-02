1329e0a639c84f8c0ee395fe2deac61b54626929
progress <nikita15933@mail.ru>
2026-06-02 19:52:17 +0300

    feat: add empty menu manager

---
+++ include/menu_manager.hpp
@@ -0,0 +1,12 @@
+
+
+namespace MenuManager
+{
+   void showMainMenu();
+   void showSettingsMenu();  
+
+   void runSettingsMenu();
+
+   int getNumber();
+   void waitForEnter();
+} // namespace MenuManager
---
+++ src/menu_manager.cpp
@@ -0,0 +1,24 @@
+#include "menu_manager.hpp"
+
+
+
+void MenuManager::showMainMenu()
+{
+}
+
+void MenuManager::showSettingsMenu()
+{
+}
+
+void MenuManager::runSettingsMenu()
+{
+}
+
+int MenuManager::getNumber()
+{
+   return 0;
+}
+
+void MenuManager::waitForEnter()
+{
+}
