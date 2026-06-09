c605c4b8a1e2539b7934ae97dc32699af58b52af
progress <nikita15933@mail.ru>
2026-06-09 18:19:19 +0300

    feat: add clear screen for exit the program

--- src/main.cpp
+++ src/main.cpp
@@ -44,6 +44,7 @@ int main(int argc, const char** argv)
          }
       case 0: // Exit
          {
+            InputManager::clearScreen();
             isRunningGame = false;
             break;
          }
