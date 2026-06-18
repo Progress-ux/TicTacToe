092f9adea2e748b6b69a9c2870522ccb985ad505
progress <nikita15933@mail.ru>
2026-06-18 14:18:54 +0300

    refactor(bot): refactor easy bot move

--- include/bot.hpp
+++ include/bot.hpp
@@ -1,3 +1,4 @@
+#include <random>
 
 class EasyBot;
 class HardBot;
@@ -6,16 +7,22 @@ class Bot {
 public:
    virtual ~Bot() = default;
 
-   virtual int move() const = 0;
+   virtual int move() = 0;
 };
 
 class EasyBot : public Bot {
+private: 
+   std::uniform_int_distribution<int> distrib;
+   std::mt19937 gen;
+
 public:
-   int move() const override;
+   EasyBot() : gen(std::random_device{}()), distrib(0, 8) {}
+
+   int move() override;
 };
 
 class HardBot : public Bot {
 public:
-   int move() const override;
+   int move() override;
 
 };
--- src/bot.cpp
+++ src/bot.cpp
@@ -1,18 +1,10 @@
 #include "bot.hpp"
-#include "random"
 
-int EasyBot::move() const
+int EasyBot::move()
 {
-   std::random_device rd;
-   std::mt19937 gen(rd());
-
-   std::uniform_int_distribution<int> distrib(0, 8);
-
-   int random_number = distrib(gen);
-
-   return random_number;
+   return distrib(gen);
 }
 
-int HardBot::move() const
+int HardBot::move()
 {
 }
