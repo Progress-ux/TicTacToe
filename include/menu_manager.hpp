a38ff758327140f0501a67d03c999cd87674a3b2
progress <nikita15933@mail.ru>
2026-06-21 17:23:03 +0300

    fix: remove settings menu

--- include/menu_manager.hpp
+++ include/menu_manager.hpp
@@ -6,15 +6,12 @@
 namespace MenuManager
 {
    void showMainMenu();
-   void showSettingsMenu(const std::string& serverIp, unsigned short serverPort);  
-
+   
    void showSinglePlayMenu();
    void runSinglePlayMenu();
 
    void showSetCurrentPlayerMenu();
    char runSetCurrentPlayerMenu();
-
-   void runSettingsMenu();
 
    void fieldRendering(std::vector<char>& cells);
    void showGetModeMenu();
